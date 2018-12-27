9b05ad3664617df3931d6daa72b36c0dc8473009
Misael Bitencourt <misael.bitencourt@santaluzia.corp>
2018-12-27 10:02:57 -0200

    responsive navbar

--- public/cms/src/components/topnav.js
+++ public/cms/src/components/topnav.js
@@ -1,9 +1,27 @@
+import icon from './icon';
 
+const fixedItems = [
+    {tag: 'li', className: 'nav-item menu-toggle pt-3 pl-4 mb-3 hidden-desktop', children: [
+        icon('menu', 32, 24)
+    ], bootstrap: el => {
+        el.addEventListener('click', () => {
+            el.parentElement.classList.toggle('mobile-open');
+        });
+    }},
+
+    {tag: 'li', className: 'nav-item menu-close pt-3 pl-4 mb-3 hidden-desktop', children: [
+        icon('cancel', 32, 24)
+    ], bootstrap: el => {
+        el.addEventListener('click', () => {
+            el.parentElement.classList.toggle('mobile-open');
+        });
+    }}
+];
 
 export default menus => ({
     tag: 'ul',
     className: 'nav nav-tabs bg-primary',
-    children: menus.map(menu => ({
+    children: fixedItems.concat(menus.map(menu => ({
         tag: 'li',
         className: 'nav-item',
         children: [
@@ -11,6 +29,6 @@ export default menus => ({
                 textContent: menu.name,
                 attrs: {href: 'javascript:;'}, on: ['click', menu.onclick], title: menu.tooltip}
         ]
-    }))
+    })))
 })
 
